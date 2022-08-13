d7232e7a5f57ba6704109e478ace41c5bc44ed73
arturnik <57458818+arturnik@users.noreply.github.com>
2022-08-13 17:03:56 +0300

    start of project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import AddUser from './components/AddUser';
 
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <AddUser />
   );
 }
 
---
+++ src/components/AddUser.js
@@ -0,0 +1,11 @@
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+
+    return (
+        <div><h2>test</h2></div>
+    );
+
+}
+
+export default AddUser;
